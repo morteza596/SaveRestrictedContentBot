3f717ce16258fb588ee783075a2dbca1eae20f20
morteza596 <115032472+morteza596@users.noreply.github.com>
2024-09-08 05:22:29 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config(66102, default=None, cast=int)
+API_ID = config("66102", default=None, cast=int)
 API_HASH = config("b6da542ebc418c1f282d372152d40664", default=None)
 BOT_TOKEN = config("7179303938:AAGwNkCqj8pBVpM-H8rkTLWYULxjVAxgmco", default=None)
 SESSION = config("BAABAjYAPnsAXsDAh-lN6sVQyBJTwOTetlPLu7P690tlBRFnSpt1nKslS_IE-3S73-qyXvxTKEFAagOIL4JTk2OexraUvQGkcrIGjiSta5Y2QIixBH-QYSVoZAvP1_xQ9rVoiGVZXQ8D2urz41big4ZH9FStVD6cqG1FgkiEWm3cEOu84-GIlt40KnkqqOXEUQPMUslHIew59_03_x3zXM612ImZ5rCWArbzR3OdTbrbrBzFzaaPBNjlPeLw-j4fb44vZm-on4kHvoznhVRKiRGuzmbGBFXSJtX1TiStEl_ZCALGBjv-vIF0qpS-EwIpHl2KqywjZWiMVdGfrhiKCYI_bijtdAAAAAGr63wCAQ", default=None)
 FORCESUB = config("caronline_original", default=None)
-AUTH = config(579247196, default=None, cast=int)
+AUTH = config("579247196", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
